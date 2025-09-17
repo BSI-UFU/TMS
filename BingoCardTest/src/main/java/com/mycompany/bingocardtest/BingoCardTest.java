a8c5309028950f6da9f6fa98a2dc1934d48c6b96
Kensley Oliveira <kensley.oliveira@ufu.br>
2025-09-16 22:25:17 -0300

    Inclusão de exercícicos das aula 4 a 7

---
+++ BingoCardTest/src/main/java/com/mycompany/bingocardtest/BingoCardTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bingocardtest;
+
+/**
+ *
+ * @author kensl
+ */
+public class BingoCardTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
